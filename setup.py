01b1390c06d515bff271585a54ae3f9666663281
Akshat Sharma <akshatsharma5877@gmail.com>
2025-10-30 10:57:59 +0530

    Initial commit with cookiecutteer template and venv setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repo will be used for end to end mlops cycle, from experiment tracking, data pipeline to CI/CD, docker and aws service.',
+    author='Akshat Sharma',
+    license='MIT',
+)
