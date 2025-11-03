ff7ca8150549ea3f5c592ecbab0e668488aad788
Akshat Sharma <akshatsharma5877@gmail.com>
2025-11-03 22:05:12 +0530

    add demo.db

--- FastApi_app/database.py
+++ FastApi_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@127.0.0.1:3306/car_test"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./demo.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
